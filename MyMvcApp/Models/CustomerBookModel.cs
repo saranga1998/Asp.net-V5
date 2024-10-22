5bd1121a5bbdd8acb09e1c5713f7d983b5436f61
Sajitha Saranga <109761136+saranga1998@users.noreply.github.com>
2024-10-22 23:26:05 +0530

    Many to Many

---
+++ MyMvcApp/Models/CustomerBookModel.cs
@@ -0,0 +1,28 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using MyMvcApp.Models;
+
+namespace MyMvcApp{
+    public class CustomerBookModel{
+
+        [Key]
+        public string BorrowId {get; set;}
+
+        public DateTime IssueDate {get; set;}
+
+        public DateTime RecievedDate {get; set;}
+
+        [ForeignKey("Book")]
+
+        public string Id {get; set;}
+
+        public BookModel Book {get; set;}
+
+        [ForeignKey("Customer")]
+
+        public string NIC {get; set;}
+
+        public CustomerModel Customer{get; set;}
+    }
+}
